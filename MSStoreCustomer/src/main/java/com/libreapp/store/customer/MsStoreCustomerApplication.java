374c1ed7c4a6337771ccf4c747a10354b7f3875d
Karc <kevinrojascobenas@gmail.com>
2021-11-12 10:16:53 -0500

    Desarrollo de EurekaServer

--- MSStoreCustomer/src/main/java/com/libreapp/store/customer/MsStoreCustomerApplication.java
+++ MSStoreCustomer/src/main/java/com/libreapp/store/customer/MsStoreCustomerApplication.java
@@ -2,16 +2,18 @@ package com.libreapp.store.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
+@EnableEurekaClient
 @SpringBootApplication
 public class MsStoreCustomerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsStoreCustomerApplication.class, args);
-		log.info("--> Load Complete!!!");
+		log.info("--> Load MSCustomer Complete!!!");
 	}
 
 }
